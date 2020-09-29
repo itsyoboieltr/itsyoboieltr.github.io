0520c9e0374c1071e811c0032e16a91de12501ec
Norbert Elter <norbert.elter@Norbert-MacBook-Pro.local>
2020-09-29 15:36:39 +0200

    Initial commit

--- mail.php
+++ mail.php
@@ -1,17 +1,21 @@
 <?php
 
-if($_POST['submit']) {
+if(isset($_POST['submit'])) {
     $recipient="norbiii@icloud.com";
     $subject="Részeg Buszsofőr Support: $sender";
-    $sender=$_POST["sender"];
-    $senderEmail=$_POST["senderEmail"];
-    $message=$_POST["message"];
+    $sender=$_POST['sender'];
+    $senderEmail=$_POST['senderEmail'];
+    $message=$_POST['message'];
 
-    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+    echo("Name: " . $sender. "<br />\\n");
+    echo("Email: " . $senderEmail. "<br />\\n");
+    echo("Problem: " . $message. "<br />\\n");
+
+    /* $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
 
     mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
 
-    $thankYou="<p>Thank you for contacting the support. We will get back to you as soon as possible.</p>";
+    $thankYou="<p>Thank you for contacting the support. We will get back to you as soon as possible.</p>"; */
 }
 
 ?>
